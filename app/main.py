7f1f3d58bdd44ce31bc9f94650d9dd3f76b584f6
Breno Oliveira Rodrigues <bor@cin.ufpe.br>
2025-07-08 00:49:16 -0300

    feat(Docker): adds support to grafana-prometheus

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
+import uvicorn
 from fastapi import FastAPI
+from starlette_prometheus import PrometheusMiddleware, metrics
 
-app = FastAPI()
 
+swagger_ui_parameters = {
+    "docExpansion": "none",
+    "displayRequestDuration": True,
+    "filter": True,
+}
 
-@app.get("/")
+app = FastAPI(
+    title="CInBora API",
+    description="API for CInBora",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
+    swagger_ui_parameters=swagger_ui_parameters,
+)
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
+
+@app.get("/", summary="Hello world Endpoint", tags=["Root"])
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="critical")
