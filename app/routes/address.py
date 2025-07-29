3132b34f223df4a76bc659a7cb642a3de45e3480
= <=>
2025-07-29 14:37:54 -0300

    feat: adjust to one address

--- app/routes/address.py
+++ app/routes/address.py
@@ -10,23 +10,18 @@ from app.types.address import AddressCreate, AddressResponse
 router = APIRouter()
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_address(address: AddressCreate, session: Session = Depends(get_session)):
+async def create_address(address_create: AddressCreate, session: Session = Depends(get_session)):
 
-    if not address.origin_address or not address.destination_address:
+    if not address_create.address:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Origin and destination are required")
 
-    origin_lat, origin_lng = await geocode_address(address.origin_address)
-
-    destination_lat, destination_lng = await geocode_address(address.destination_address)
+    lat, lng = await geocode_address(address_create.address)
     
     new_address = Address(
         id=str(uuid4()),
-        origin_address=address.origin_address,
-        destination_address=address.destination_address,
-        origin_lat=origin_lat,
-        origin_lng=origin_lng,
-        destination_lat=destination_lat,
-        destination_lng=destination_lng,
+        address=address_create.address,
+        latitude=lat,
+        longitude=lng,
     )
 
     session.add(new_address)
